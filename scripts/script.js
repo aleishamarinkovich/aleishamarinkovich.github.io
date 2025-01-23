5787f20dff9100b7d8bb584570d5277a81167a6a
Xavier Kirkpatrick <kirkpatrickxavier@gmail.com>
2025-01-23 16:54:14 +1300

    add correct text layout via wrapping letters inside spans for each word and remove whitespace

--- scripts/script.js
+++ scripts/script.js
@@ -2,20 +2,28 @@ import DOMpurify from 'dompurify';
 
 document.addEventListener('DOMContentLoaded', () => {
   // Select the element containing the text and
-  // Wrap each character in a <span> for individual animation
+  // wrap each character in a <span> for individual animation
   function wrapTextInSpans(ids) {
     ids.forEach((id) => {
       const getElement = document.getElementById(id);
       if (!getElement) {
         console.error(`Element with ID ${id} not found.`);
       }
-      const wrappedText = getElement.textContent
-        .split('')
-        .map((char) => {
-          // Preserve spaces by converting them to non-breaking spaces
-          return char === ' ' ? ' ' : `<span class="letter">${char}</span>`;
+      const textContent = getElement.textContent
+        .replace(/\s*\n\s*/g, ' ')
+        .replace(/\s+/g, ' ')
+        .trim();
+      const wrappedText = textContent
+        .split(' ')
+        .map((word) => {
+          const wrappedWord = [...word]
+            .map((char) => `<span class="letter">${char}</span>`)
+            .join('');
+          return wrappedWord === ' '
+            ? ' '
+            : `<span class="word">${wrappedWord}</span>`;
         })
-        .join('');
+        .join(' ');
       // DOMpurify library used to sanitize the HTML content & prevent XSS attacks.
       const cleanText = DOMpurify.sanitize(wrappedText);
       getElement.innerHTML = cleanText;
@@ -24,7 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   wrapTextInSpans(['name', 'bio', 'italic']);
 
-  // Add mouse event listeners
+  // Add mouse-enter event-listeners to each letter.
   const letters = document.querySelectorAll('.letter');
   letters.forEach((letter) => {
     letter.addEventListener('mouseenter', () => {
